d6eef38f11053f8647146d40a95d715a00f8ee16
Lerxifer <luoxi20030116@163.com>
2026-04-26 11:55:09 +0800

    update literature data with 21 papers

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,211 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "addis2024evaluating": {
+        "author": "Addis, Bernardetta and Carello, Giuliana and Tanfani, Elena",
+        "booktitle": "Healthcare",
+        "number": "20",
+        "organization": "MDPI",
+        "pages": "2023",
+        "title": "Evaluating the impact of the level of robustness in operating room scheduling problems",
+        "type": "inproceedings",
+        "volume": "12",
+        "year": "2024"
+    },
+    "aldomi2026deep": {
+        "author": "Aldomi, Saba and Suleiman, Husam and Shatnawi, Ali and Alawneh, Luay",
+        "journal": "Systems and Soft Computing",
+        "pages": "200442",
+        "publisher": "Elsevier",
+        "title": "A deep learning approach for early prediction of task failures in cloud computing environments",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
+        "year": "2026"
+    },
+    "azarian2025cba": {
+        "author": "Azarian, Talin and Yadollahzadeh-Tabari, Meisam and Bozorgi-Rad, Yaser",
+        "journal": "Cluster Computing",
+        "number": "13",
+        "pages": "855",
+        "publisher": "Springer",
+        "title": "CBA-HDL: A hybrid deep learning model for scalable and efficient cloud workload forecasting with enhanced accuracy",
+        "type": "article",
+        "volume": "28",
+        "year": "2025"
+    },
+    "bawa2025improving": {
+        "author": "Bawa, Jyoti and Kaur Chahal, Kuljit and Kaur, Kamaljit",
+        "journal": "The Journal of Supercomputing",
+        "number": "10",
+        "pages": "1138",
+        "publisher": "Springer",
+        "title": "Improving cloud resource management: an ensemble learning approach for workload prediction: J. Bawa et al.",
+        "type": "article",
+        "volume": "81",
+        "year": "2025"
+    },
+    "bhardwaj2025deep": {
+        "author": "Bhardwaj, Aditya and Sai, Yashwant Ram and Patil, Nilesh Vishwasrao",
+        "booktitle": "2025 IEEE International Conference on Electronics, Computing and Communication Technologies (CONECCT)",
+        "organization": "IEEE",
+        "pages": "1--6",
+        "title": "Deep Learning Models for Workload Prediction in Virtual Machine Cloud Data Center",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "bi2021integrated": {
+        "author": "Bi, Jing and Li, Shuang and Yuan, Haitao and Zhou, MengChu",
+        "journal": "Neurocomputing",
+        "pages": "35--48",
+        "publisher": "Elsevier",
+        "title": "Integrated deep learning method for workload and resource prediction in cloud systems",
+        "type": "article",
+        "volume": "424",
+        "year": "2021"
+    },
+    "brandao2016bin": {
+        "author": "Brandao, Filipe and Pedroso, Joao Pedro",
+        "journal": "Computers \\& Operations Research",
+        "pages": "56--67",
+        "publisher": "Elsevier",
+        "title": "Bin packing and related problems: General arc-flow formulation with graph compression",
+        "type": "article",
+        "volume": "69",
         "year": "2016"
+    },
+    "calheiros2014workload": {
+        "author": "Calheiros, Rodrigo N and Masoumi, Enayat and Ranjan, Rajiv and Buyya, Rajkumar",
+        "journal": "IEEE transactions on cloud computing",
+        "number": "4",
+        "pages": "449--458",
+        "publisher": "IEEE",
+        "title": "Workload prediction using ARIMA model and its impact on cloud applications\u2019 QoS",
+        "type": "article",
+        "volume": "3",
+        "year": "2014"
+    },
+    "de2025integration": {
+        "author": "de Melo, Igor Eduardo Santos and Fogliatto, Flavio Sanson",
+        "journal": "Computers \\& Operations Research",
+        "pages": "107063",
+        "publisher": "Elsevier",
+        "title": "Integration of decision levels in operating room scheduling problems: Systematic review and proposition of a decision support framework",
+        "type": "article",
+        "volume": "180",
+        "year": "2025"
+    },
+    "delorme2016bin": {
+        "author": "Delorme, Maxence and Iori, Manuel and Martello, Silvano",
+        "journal": "European Journal of Operational Research",
+        "number": "1",
+        "pages": "1--20",
+        "publisher": "Elsevier",
+        "title": "Bin packing and cutting stock problems: Mathematical models and exact algorithms",
+        "type": "article",
+        "volume": "255",
+        "year": "2016"
+    },
+    "deshmukh2025coordinate": {
+        "author": "Deshmukh, Hrushikesh",
+        "booktitle": "2025 International Conference on Intelligent Systems and Computational Networks (ICISCN)",
+        "organization": "IEEE",
+        "pages": "1--6",
+        "title": "Coordinate Attention Mechanism based Gated Recurrent Unit for Workload Prediction in Cloud Computing",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "hu2017branch": {
+        "author": "Hu, Qian and Zhu, Wenbin and Qin, Hu and Lim, Andrew",
+        "journal": "European Journal of Operational Research",
+        "number": "1",
+        "pages": "70--80",
+        "publisher": "Elsevier",
+        "title": "A branch-and-price algorithm for the two-dimensional vector packing problem with piecewise linear cost function",
+        "type": "article",
+        "volume": "260",
+        "year": "2017"
+    },
+    "kashyap2025hybrid": {
+        "author": "Kashyap, Shobhana and Singh, Avtar",
+        "journal": "Journal of Grid Computing",
+        "number": "3",
+        "pages": "22",
+        "publisher": "Springer",
+        "title": "A hybrid scheduling for multi-objective optimization using prediction approach",
+        "type": "article",
+        "volume": "23",
+        "year": "2025"
+    },
+    "rachuba2017fuzzy": {
+        "author": "Rachuba, Sebastian and Werners, Brigitte",
+        "journal": "Annals of Operations Research",
+        "number": "1",
+        "pages": "325--350",
+        "publisher": "Springer",
+        "title": "A fuzzy multi-criteria approach for robust operating room schedules",
+        "type": "article",
+        "volume": "251",
+        "year": "2017"
+    },
+    "rana2025block": {
+        "author": "Rana, Vishrut and Yano, Candace Arai and Newman, Alexandra M",
+        "journal": "Operations Research, Data Analytics and Logistics",
+        "pages": "200477",
+        "publisher": "Elsevier",
+        "title": "Block scheduling in a multi-hospital setting",
+        "type": "article",
+        "volume": "45",
+        "year": "2025"
+    },
+    "samudra2016scheduling": {
+        "author": "Samudra, Michael and Van Riet, Carla and Demeulemeester, Erik and Cardoen, Brecht and Vansteenkiste, Nancy and Rademakers, Frank E",
+        "journal": "Journal of scheduling",
+        "number": "5",
+        "pages": "493--525",
+        "publisher": "Springer",
+        "title": "Scheduling operating rooms: achievements, challenges and pitfalls",
+        "type": "article",
+        "volume": "19",
+        "year": "2016"
+    },
+    "valerio1999exact": {
+        "author": "Val{\\'e}rio de Carvalho, JM",
+        "journal": "Annals of Operations Research",
+        "number": "0",
+        "pages": "629--659",
+        "publisher": "Springer",
+        "title": "Exact solution of bin-packing problems using column generation and branch-and-bound",
+        "type": "article",
+        "volume": "86",
+        "year": "1999"
+    },
+    "wei2020new": {
+        "author": "Wei, Lijun and Luo, Zhixing and Baldacci, Roberto and Lim, Andrew",
+        "journal": "INFORMS Journal on Computing",
+        "number": "2",
+        "pages": "428--443",
+        "publisher": "INFORMS",
+        "title": "A new branch-and-price-and-cut algorithm for one-dimensional bin-packing problems",
+        "type": "article",
+        "volume": "32",
+        "year": "2020"
+    },
+    "zhang2026online": {
+        "author": "Zhang, Huayan and Liu, Tie-Yan and Bai, Ruibin",
+        "journal": "Expert Systems with Applications",
+        "pages": "131074",
+        "publisher": "Elsevier",
+        "title": "Online risk-aware pattern adjustment for bin packing problem",
+        "type": "article",
+        "volume": "308",
+        "year": "2026"
+    },
+    "zhu2019operating": {
+        "author": "Zhu, Shuwan and Fan, Wenjuan and Yang, Shanlin and Pei, Jun and Pardalos, Panos M",
+        "journal": "Journal of Combinatorial Optimization",
+        "number": "3",
+        "pages": "757--805",
+        "publisher": "Springer",
+        "title": "Operating room planning and surgical case scheduling: a review of literature",
+        "type": "article",
+        "volume": "37",
+        "year": "2019"
     }
 };
